810dc401386506f6eb4a952e433ab341762b3e85
sumeshi <j15322sn@gmail.com>
2020-11-02 09:13:41 +0900

    feat: parse

--- src/valetsshing/valetsshing.py
+++ src/valetsshing/valetsshing.py
@@ -1,5 +1,58 @@
 # coding: utf-8
+import re
+from pathlib import Path
+from dataclasses import dataclass, field
+
+from typing import Optional, List
+
 import click
+
+@dataclass
+class SshConfigDir:
+    path: Path
+
+
+@dataclass
+class SshConfig:
+    name: str
+    hostname: str
+    user: str
+    identity_file: Optional[Path] = None
+    optional_settings: dict = field(default_factory=dict)
+
+
+def gen_parsed_ssh_config(config_path: Path) -> List[SshConfig]:
+    text = config_path.read_text()
+    split_pattern = re.compile(r'\n\s?\n+')
+
+    for conf in re.split(split_pattern, text):
+        sshconfig = parse_ssh_config(conf)
+        if sshconfig:
+            print(sshconfig)
+
+
+def match_attr(attr: str, text: str) -> Optional[str]:
+    if re.match(fr'^{attr}\s', text, flags=re.IGNORECASE):
+        parts = text.split(' ')
+        if len(parts) == 2:
+            return parts[1]
+    return None
+
+
+def parse_ssh_config(text: str) -> SshConfig:
+
+    attr_dict: dict = {}
+
+    for line in text.splitlines():
+        line = line.strip()
+
+        for attr in ('host', 'hostname', 'user', 'identityfile'):
+            find_attr = match_attr(attr, line)
+            if find_attr:
+                attr_dict[attr] = find_attr
+        
+    return {key: value for key, value in attr_dict.items() if value}
+
 
 @click.group()
 def valetsshing():
@@ -11,7 +64,7 @@ def add():
 
 @valetsshing.command()
 def lst():
-    print('list valetsshing')
+    gen_parsed_ssh_config(Path('/Users/s.nakano/.ssh/config'))
 
 
 def run():
